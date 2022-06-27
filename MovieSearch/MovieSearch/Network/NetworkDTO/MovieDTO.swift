cdeb30223371e306156b5349914e378d8b14d5ec
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 16:50:55 +0900

    feat: DTO를 변환하기 위한 toDomain 메서드 구현 #1

--- MovieSearch/MovieSearch/Network/NetworkDTO/MovieDTO.swift
+++ MovieSearch/MovieSearch/Network/NetworkDTO/MovieDTO.swift
@@ -4,6 +4,21 @@ struct MovieDTO: Codable {
     let title: String
     let link: String
     let image: String
-    let subtitle, pubDate, director, actor: String
+    let subtitle: String
+    let pubDate: String
+    let director: String
+    let actor: String
     let userRating: String
+    
+    func toDomain() -> Movie {
+        return Movie(
+            title: title,
+            link: link,
+            image: image,
+            subtitle: subtitle,
+            pubDate: pubDate,
+            director: director,
+            actor: actor,
+            userRating: userRating)
+    }
 }
--- MovieSearch/MovieSearch/Network/NetworkDTO/MovieSearhInformationDTO.swift
+++ MovieSearch/MovieSearch/Network/NetworkDTO/MovieSearhInformationDTO.swift
@@ -2,6 +2,19 @@ import Foundation
 
 struct MovieSearhInformationDTO: Codable {
     let lastBuildDate: String
-    let total, start, display: Int
+    let total: Int
+    let start: Int
+    let display: Int
     let items: [MovieDTO]
+    
+    func toDomain() -> MovieSearhInformation {
+        let movies = items.map { $0.toDomain() }
+        return MovieSearhInformation(
+            lastBuildDate: lastBuildDate,
+            total: total,
+            start: start,
+            display: display,
+            items: movies
+            )
+    }
 }
