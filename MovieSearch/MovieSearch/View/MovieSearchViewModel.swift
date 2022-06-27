cce9e5941c791eab9527408443c0a9f0fbc1fde4
ICS-Asan <carpediem1015@gmail.com>
2022-06-28 03:37:54 +0900

    feat: 검색결과가 나타날 수 있도록 View-ViewModel 연결 #3 #4

--- MovieSearch/MovieSearch/View/MovieSearchViewModel.swift
+++ MovieSearch/MovieSearch/View/MovieSearchViewModel.swift
@@ -3,9 +3,44 @@ import RxSwift
 
 final class MovieSearchViewModel {
     private let movieSearchUseCase = MovieSearchUseCase()
-    private(set) var searchResult: [Movie] = []
+    private let disposeBag: DisposeBag = .init()
+    private(set) var searchResults: [Movie] = []
+    private(set) var totalResultCount: Int = 0
+    
+    
+    func transform(_ input: Input) -> Output {
+        input
+            .searchMovieObserver
+            .subscribe(onNext: { [weak self] data in
+                self?.storeSearchResult(with: data)
+            })
+            .disposed(by: disposeBag)
+
+        return Output()
+    }
+    
+    func storeSearchResult(with movieInformation: MovieSearchInformation?) {
+        guard let movieInformation = movieInformation else { return }
+        totalResultCount = movieInformation.total
+        searchResults = movieInformation.items
+    }
     
     func fetchSearchResult(with word: String) -> Observable<MovieSearchInformation?> {
         movieSearchUseCase.fetchMovieSearchInformation(with: word)
     }
+}
+
+extension MovieSearchViewModel {
+    final class Input {
+        let searchMovieObserver: Observable<MovieSearchInformation?>
+
+        init(
+            searchMovieObserver: Observable<MovieSearchInformation?>
+        ) {
+            self.searchMovieObserver = searchMovieObserver
+        }
+    }
+
+    final class Output { }
+
 }
