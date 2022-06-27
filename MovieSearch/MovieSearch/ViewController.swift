08f4f5ba5890a84fe8e59f4f6d1b0515389cd94b
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 15:31:04 +0900

    file: 프로젝트 파일 생성 및 Swift 버전 설정 #5
    - SwiftVersion: 5

---
+++ MovieSearch/MovieSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Seul Mac on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
