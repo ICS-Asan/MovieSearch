f0d58999d7bc2f6dfb8e1da1d36e998c304c436f
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 20:25:52 +0900

    feat: MovieCell 내부 요소 AutoLayout 설정 #7

--- MovieSearch/MovieSearch/View/MovieCell.swift
+++ MovieSearch/MovieSearch/View/MovieCell.swift
@@ -1,4 +1,6 @@
 import UIKit
+import SnapKit
+import SDWebImage
 
 final class MovieCell: UICollectionViewCell {
     
@@ -55,9 +57,51 @@ final class MovieCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupPosterImageViewLayout()
+        setupTextInformationStackView()
+        setupBookmarkButtonLayout()
+    }
+    
+    private func setupPosterImageViewLayout() {
+        contentView.addSubview(posterImageView)
+        posterImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.top.bottom.equalToSuperview().inset(5)
+            make.height.equalTo(100)
+            make.width.equalTo(posterImageView.snp.height).multipliedBy(0.7)
+        }
+    }
+    
+    private func setupTextInformationStackViewLayout() {
+        setupTextInformationStackView()
+        contentView.addSubview(textInformationStackView)
+        textInformationStackView.snp.makeConstraints { make in
+            make.leading.equalTo(posterImageView.snp.trailing).inset(-10)
+            make.top.bottom.equalTo(posterImageView)
+            make.trailing.equalToSuperview()
+        }
+    }
+    
+    private func setupTextInformationStackView() {
+        textInformationStackView.addArrangedSubview(titleLabel)
+        textInformationStackView.addArrangedSubview(directorLabel)
+        textInformationStackView.addArrangedSubview(actorLabel)
+        textInformationStackView.addArrangedSubview(userRatingLabel)
+    }
+    
+    private func setupBookmarkButtonLayout() {
+        contentView.addSubview(bookmarkButton)
+        bookmarkButton.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview().inset(5)
+        }
     }
 }
