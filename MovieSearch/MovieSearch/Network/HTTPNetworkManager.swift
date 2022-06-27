7856a6277d11e174a3ad2991a584ce232280ec65
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 17:05:43 +0900

    feat: createURLRequest 구현 #1

--- MovieSearch/MovieSearch/Network/HTTPNetworkManager.swift
+++ MovieSearch/MovieSearch/Network/HTTPNetworkManager.swift
@@ -7,4 +7,15 @@ final class HTTPNetworkManager {
     init(urlSession: URLSession = URLSession.shared) {
         self.urlSession = urlSession
     }
+    
+    func createURLRequest(with endPoint: URL?) -> URLRequest? {
+        guard let endPoint = endPoint else {
+            return nil
+        }
+        var urlRequest = URLRequest(url: endPoint, method: .get)
+        urlRequest.setValue("X-Naver-Client-Id", forHTTPHeaderField: "xIDxQUaIwFGS6sVcbAyN")
+        urlRequest.addValue("X-Naver-Client-Secret", forHTTPHeaderField: "mJPepkAAi3")
+        
+        return urlRequest
+    }
 }
