f8dd4ceeeabdc812531310ae0637e0ef8fafed69
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 16:26:08 +0900

    refactor: 검색 결과를 20개 까지 표시하기 위해 쿼리 추가 #1

--- MovieSearch/MovieSearch/Network/EndPoint.swift
+++ MovieSearch/MovieSearch/Network/EndPoint.swift
@@ -3,14 +3,15 @@ import Foundation
 enum EndPoint {
     private static let mainPath = "    https://openapi.naver.com/v1/search/movie.json"
     
-    case movieSearch(word: String)
+    case movieSearch(word: String, displayCount: Int = 20)
 
     var url: URL? {
         switch self {
-        case .movieSearch(let word):
+        case .movieSearch(let word, let display):
             var components = URLComponents(string: EndPoint.mainPath)
             let searchWord = URLQueryItem(name: "query", value: "\(word)")
-            components?.queryItems = [searchWord]
+            let display = URLQueryItem(name: "display", value: "\(display)")
+            components?.queryItems = [searchWord, display]
             return components?.url
         }
     }
