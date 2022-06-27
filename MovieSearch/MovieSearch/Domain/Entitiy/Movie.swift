047e4104bee11151dbbaafa7ff7d4d6b145b4d8b
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 16:46:30 +0900

    feat: Entity의 모델 타입 구현 #1

---
+++ MovieSearch/MovieSearch/Domain/Entitiy/Movie.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Movie {
+    let title: String
+    let link: String
+    let image: String
+    let subtitle: String
+    let pubDate: String
+    let director: String
+    let actor: String
+    let userRating: String
+}
---
+++ MovieSearch/MovieSearch/Domain/Entitiy/MovieSearchInformation.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct MovieSearhInformation {
+    let lastBuildDate: String
+    let total: Int
+    let start: Int
+    let display: Int
+    let items: [Movie]
+}
