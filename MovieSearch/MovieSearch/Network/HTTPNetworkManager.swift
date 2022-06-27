4d275a411771f97bb06588b155c17eacc33348b8
ICS-Asan <carpediem1015@gmail.com>
2022-06-27 17:14:14 +0900

    feat: fetch 메서드 구현 #1

--- MovieSearch/MovieSearch/Network/HTTPNetworkManager.swift
+++ MovieSearch/MovieSearch/Network/HTTPNetworkManager.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RxSwift
 
 final class HTTPNetworkManager {
     static let shared = HTTPNetworkManager()
@@ -23,5 +24,31 @@ final class HTTPNetworkManager {
         )
         
         return urlRequest
+    }
+    
+    func fetch(with endPoint: URL?) -> Observable<Data> {
+        guard let endPoint = endPoint else {
+            return .error(fatalError())
+        }
+
+        return Observable.create() { [weak self] emitter in
+            let urlRequest = URLRequest(url: endPoint, method: .get)
+            let task = self?.urlSession.dataTask(with: urlRequest) { (data, response , error) in
+                guard error == nil else {
+                    emitter.onError(fatalError())
+                    return
+                }
+
+                if let data = data {
+                    emitter.onNext(data)
+                }
+                emitter.onCompleted()
+            }
+            task?.resume()
+
+            return Disposables.create() {
+                task?.cancel()
+            }
+        }
     }
 }
