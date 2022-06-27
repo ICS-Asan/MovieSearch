0cc491a8ce4e0502b53fc98ce9ac000ffce5bbb9
ICS-Asan <carpediem1015@gmail.com>
2022-06-28 07:46:02 +0900

    feat: loadPersistentContainer 구현 #13

--- MovieSearch/MovieSearch/Data/CoreData/CoreDataManager.swift
+++ MovieSearch/MovieSearch/Data/CoreData/CoreDataManager.swift
@@ -4,10 +4,20 @@ import RxSwift
 
 final class CoreDataManager {
     static let shared = CoreDataManager()
-    private init() { }
-    
     private let persistentContainer = NSPersistentCloudKitContainer(name: "MovieCoreDataDTO")
     private(set) lazy var context = persistentContainer.viewContext
+    
+    private init() {
+        loadPersistentContainer()
+    }
+    
+    private func loadPersistentContainer() {
+        persistentContainer.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
     
     func fetch() -> Observable<[Movie]> {
         return Observable.create { [weak self] emitter in
