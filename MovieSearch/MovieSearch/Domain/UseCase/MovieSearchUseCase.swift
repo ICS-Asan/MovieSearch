4aac2b3b485ab400442905672413058701715bc2
ICS-Asan <carpediem1015@gmail.com>
2022-06-28 08:23:18 +0900

    feat: MovieSearchUseCase에 CoreData 적용 #13

--- MovieSearch/MovieSearch/Domain/UseCase/MovieSearchUseCase.swift
+++ MovieSearch/MovieSearch/Domain/UseCase/MovieSearchUseCase.swift
@@ -3,12 +3,29 @@ import RxSwift
 
 final class MovieSearchUseCase {
     let movieNetworkRepository: NetworkRepository
+    let coreDataRepository: CoreDataRepository
     
-    init(movieNetworkRepository: NetworkRepository = MovieNetworkRepository()) {
+    init(
+        movieNetworkRepository: NetworkRepository = MovieNetworkRepository(),
+        coreDataRepository: CoreDataRepository = MovieCoreDataRepository()
+    ) {
         self.movieNetworkRepository = movieNetworkRepository
+        self.coreDataRepository = coreDataRepository
     }
     
     func fetchMovieSearchInformation(with searchWord: String) -> Observable<MovieSearchInformation?> {
         return movieNetworkRepository.fetchMovieSearchInformation(with: searchWord)
+    }
+    
+    func fetchBookmarkedMovie() -> Observable<[Movie]> {
+        return coreDataRepository.fetch()
+    }
+    
+    func saveBookmarkedMovie(_ movie: Movie) {
+        coreDataRepository.save(movie)
+    }
+    
+    func deleteBookmarkedMovie(with title: String) {
+        coreDataRepository.delete(with: title)
     }
 }
