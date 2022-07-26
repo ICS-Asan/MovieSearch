416aa9b73e9a8b4bbcc3454a6b3430154cf13f93
ICS-Asan <carpediem1015@gmail.com>
2022-07-26 16:44:40 +0900

    feat: 각 화면별 navigationContoller 생성 및 init 생성 #29

--- MovieSearch/MovieSearch/View/MovieTabBarController.swift
+++ MovieSearch/MovieSearch/View/MovieTabBarController.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 class MovieTabBarController: UITabBarController {
-
+    let searchNavigationController = UINavigationController(rootViewController: MovieSearchViewController())
+    let bookmarkNavigationController = UINavigationController(rootViewController: BookmarkListViewController())
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
